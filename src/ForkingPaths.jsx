70027dddf6d3164166b7289885c87d3ee3cf4fe1
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2024-10-26 21:17:15 -0400

    context menu delets or duplicates a clicked module

--- src/ForkingPaths.jsx
+++ src/ForkingPaths.jsx
@@ -1,17 +1,18 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback, useState, useEffect, useRef } from 'react';
 import ReactFlow, {
   addEdge,
   useEdgesState,
   useNodesState,
   Background,
   Controls,
+  MarkerType
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import './ForkingPaths.css';
 
 import CustomNode from './components/CustomNode';
 
-
+import ContextMenu from './components/UI/ContextMenu';
 
 const nodeTypes = { customNode: CustomNode };
 
@@ -29,6 +30,8 @@ function App() {
 
     const [clickedEdge, setClickedEdge] = useState(null); // State for clicked edge
 
+    const [menu, setMenu] = useState(null);
+    const contextRef = useRef(null);
 
         function getEdgeColor(){
             const edgeColors = [ '#FF3333', '#080FF', '#00FF00', '#9933FF', '#CCCC00', '#FF00FF']
@@ -41,6 +44,46 @@ function App() {
         []
     );
     
+
+    /* 
+    
+        EDITOR UI
+    */
+
+    // Nodes context menu 
+    const onNodeContextMenu = useCallback(
+        (event, node) => {
+            // Prevent native context menu from showing
+            event.preventDefault();
+    
+            // Calculate position of the context menu. We want to make sure it
+            // doesn't get positioned off-screen.
+            const pane = contextRef.current.getBoundingClientRect();
+            setMenu({
+            id: node.id,
+            top: event.clientY < pane.height - 100 && event.clientY,
+            left: event.clientX < pane.width - 200 && event.clientX,
+            right: event.clientX >= pane.width - 200 && pane.width - event.clientX,
+            bottom:
+                event.clientY >= pane.height - 200 && pane.height - event.clientY,
+            });
+        },
+        [setMenu],
+    );
+
+    // Close the context menu if it's open whenever the window is clicked.
+    const onPaneClick = useCallback(() => setMenu(null), [setMenu]);
+
+    
+    // Toggle sidebar collapse
+    const toggleSidebar = () => {
+        setIsSidebarCollapsed((prev) => !prev);
+    };
+
+    /* 
+
+        PATCHING
+    */
     // Function to add a new custom node
     const addCustomNode = () => {
         const newNode = {
@@ -50,11 +93,6 @@ function App() {
         data: { label: `Custom Node ${nodes.length + 1}` },
         };
         setNodes((nds) => nds.concat(newNode));
-    };
-
-    // Toggle sidebar collapse
-    const toggleSidebar = () => {
-        setIsSidebarCollapsed((prev) => !prev);
     };
 
     // Handle delete key press to remove the clicked edge
@@ -117,8 +155,8 @@ function App() {
                 Add Custom Node
             </button>
 
-            <p>To delete cables: click a cable and press 'delete' key</p>
-            
+            <p>To delete cables: click a cable and press 'delete' or 'backspace' key</p>
+            <p>Right click a module to delete or duplicate it</p>
           {/* Placeholder for Additional Components */}
           {/* Future components can be added here as more collapsible sections */}
         </div>
@@ -146,6 +184,14 @@ function App() {
         </button>
 
         <ReactFlow
+
+            // EDITOR UI
+            ref={contextRef}
+            onPaneClick={onPaneClick}
+            onNodeContextMenu={onNodeContextMenu}
+            fitView
+
+            // PATCHING
             nodes={nodes}
             edges={edges}
             onNodesChange={onNodesChange}
@@ -153,11 +199,17 @@ function App() {
             // onEdgesDelete={onEdgesDelete} // Add edge deletion handler
             onEdgeClick={onEdgeClick}
             onConnect={onConnect}
-            fitView
             nodeTypes={nodeTypes}
             deleteKeyCode={46} // Set Delete key (keyCode 46) for edge deletion
+
+
+            
+
+
+
         >
             <Background variant="dots" />
+            {menu && <ContextMenu onClick={onPaneClick} {...menu} />}
             <Controls />
         </ReactFlow>
         </div>
